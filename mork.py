566e141aa96a326be48ecfa955a05cb18ab993ab
Mark Cockburn <Mork@TheMork.co.uk>
2016-11-10 11:30:06 +0000

    fix indentation

--- mork.py
+++ mork.py
@@ -1,13 +1,12 @@
 _store = {}
- _store['input'] = input
- 
- print('MorkPy imported. https://github.com/MorkHub/MorkPy')
- def input (prompt='Input a number: ',type=int,error='Error - Wrong Type'):
-      temp = None
-      while temp is None:
-          try:
-              temp = type(_store['input'](str(prompt)))
-          except ValueError:
-              print(error)
-              continue
-      return temp
+_store['input'] = input
+print('MorkPy imported. https://github.com/MorkHub/MorkPy')
+def input (prompt='Input a number: ',type=int,error='Error - Wrong Type'):
+    temp = None
+        while temp is None:
+            try:
+                temp = type(_store['input'](str(prompt)))
+            except ValueError:
+            print(error)
+        continue
+    return temp

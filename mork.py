c0c5f0ac2d11af881f8727214e8c083d71b4d4b0
MorkHub <mork@themork.co.uk>
2016-11-29 16:54:25 +0000

    added useless class 'mork.rand': mathematical operations return randomised operations

--- mork.py
+++ mork.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 _store = {}
 _store['input'] = input
 print('MorkPy imported. https://github.com/MorkHub/MorkPy')
@@ -14,3 +16,48 @@ def input (prompt='Input a number: ',type=int,error='Error - Wrong Type',ascii=F
                 temp = None
                 continue
     return temp
+
+def random_oper(min=0,max=100):
+    x = randrange(min,max)
+    y = randrange(min,max)
+    opers = ['+','-','*','/','**']
+    oper = randrange(len(opers))
+    return eval ('{}{}{}'.format(x,opers[oper],y))
+
+class MyObject:
+    def __add__ (self, other):
+        return random_oper()
+    def __sub__ (self,other):
+        return random_oper()
+    def __mul__ (self,other):
+        return random_oper()
+    def __div__ (self,other):
+        return random_oper()
+    def __truediv__ (self,other):
+        return random_oper()
+    def __mod__ (self,other):
+        return random_oper()
+    def __pow__ (self,other):
+        return random_oper()
+
+    def __lt__ (self,other):
+        return True;
+    def __gt__ (self,other):
+        return True
+    def __gt__ (self,other):
+        return True
+    def __eq__ (self,other):
+        return True
+    def __ne__ (self,other):
+        return True
+    def __le__ (self,other):
+        return True
+    def __ge__ (self,other):
+        return True
+
+    def __str__(self):
+        return 'True'
+    def __int__(self):
+        return 1
+    def __float__(self):
+        return 1.0
